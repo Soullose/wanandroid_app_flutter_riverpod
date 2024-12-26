255885ba4bdf6a6188a7447f7e09ad85ca75e547
WangSF <soullose@qq.com>
2024-12-26 14:05:03 +0800

    feat(net): 添加 HTTP 请求日志记录
    - 在 HTTP 客户端中集成 pretty_dio_logger- 配置日志记录选项，包括请求头、请求体、响应体等
    - 设置在调试模式下启用日志记录
    - 过滤掉包含特定路径的请求和包含 Uint8List 数据的响应

--- lib/common/net/http_client.dart
+++ lib/common/net/http_client.dart
@@ -1,4 +1,6 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'result_data.dart';
 
@@ -20,6 +22,25 @@ class HttpManager extends _$HttpManager {
     // dio.interceptors.add(TokenInterceptors(ref: ref));
     dio.interceptors.add(ErrorInterceptors());
     dio.interceptors.add(ResponseInterceptors());
+    dio.interceptors.add(PrettyDioLogger(
+        requestHeader: true,
+        requestBody: true,
+        responseBody: true,
+        responseHeader: false,
+        error: true,
+        compact: true,
+        maxWidth: 90,
+        enabled: kDebugMode,
+        filter: (options, args){
+          // don't print requests with uris containing '/posts'
+          // if(options.path.contains('/posts')){
+          //   return false;
+          // }
+          // don't print responses with unit8 list data
+          return !args.isResponse || !args.hasUint8ListData;
+        }
+    )
+    );
   }
 
   static const contentTypeJson = "application/json";
