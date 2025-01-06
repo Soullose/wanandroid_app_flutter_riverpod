887e9304710acab326d05c87db002964ed8bb297
WangSF <soullose@qq.com>
2025-01-06 20:52:37 +0800

    feat(model): 添加分页数据状态提供器
    - 新增 PaginationDataState 类，用于处理分页数据的状态
    - 使用 Riverpod 注解创建分页数据状态提供器- 该类继承自 _$PaginationDataState，并重写了 build 方法

---
+++ lib/model/pagination_data_state_provider.dart
@@ -0,0 +1,11 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'pagination_data_state_provider.g.dart';
+
+@riverpod
+class PaginationDataState<T> extends _$PaginationDataState {
+  @override
+  FutureOr<PaginationDataState<T>?> build() {
+    return null;
+  }
+}
