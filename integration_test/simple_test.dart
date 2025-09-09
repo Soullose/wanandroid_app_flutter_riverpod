3e125e7cbf2dd73ea2339ef032253166d1096dd8
WangSF <soullose@qq.com>
2025-09-09 10:26:40 +0800

    test: 更新测试文件中的 Widget 名称
    - 将 simple_test.dart 和 widget_test.dart 中的 MyApp替换为 App
    - 此修改与源代码中的 App 名称

--- integration_test/simple_test.dart
+++ integration_test/simple_test.dart
@@ -1,13 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:wanandroid_app_flutter_riverpod/main.dart';
-import 'package:wanandroid_app_flutter_riverpod/src/rust/frb_generated.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:wanandroid_app_flutter_riverpod/app.dart';
+import 'package:wanandroid_app_flutter_riverpod/src/rust/frb_generated.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   setUpAll(() async => await RustLib.init());
   testWidgets('Can call rust function', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const App());
     expect(find.textContaining('Result: `Hello, Tom!`'), findsOneWidget);
   });
 }
