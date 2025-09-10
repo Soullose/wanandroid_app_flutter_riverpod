8471201e8fd6a4325085b53974f8094caa8eca3d
WangSF <soullose@qq.com>
2025-09-10 09:53:28 +0800

    feat(logging): 添加日志记录功能并优化错误处理
    - 在 pubspec.yaml 中添加 logging 依赖
    - 在 bootstrap.dart 中初始化 Logger 并配置日志级别和监听器
    - 使用 runZonedGuarded 包装 bootstrap 和 main 函数以捕获和记录未处理的错误
    - 调整 main.dart 中的启动逻辑，使用 runZonedGuarded 调用 bootstrap

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,17 @@
 // The original content is temporarily commented out to allow generating a self-contained demo - feel free to uncomment later.
 
+import 'dart:async';
+import 'dart:developer';
+
 import 'bootstrap.dart';
 
-Future<void> main() async {
-  // unawaited(
-  //   bootstrap(),
-  // );
-  await bootstrap();
-  // runZonedGuarded(
-  //   () async {
-  //     await bootstrap();
-  //   },
-  //   (error, stack) {
-  //     return log(error.toString(), stackTrace: stack);
-  //   },
-  // );
+void main() {
+  runZonedGuarded(
+    () async {
+      await bootstrap();
+    },
+    (error, stack) {
+      return log(error.toString(), stackTrace: stack);
+    },
+  );
 }
