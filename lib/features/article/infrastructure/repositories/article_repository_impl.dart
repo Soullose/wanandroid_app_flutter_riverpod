4a9d2ed0482b8547d08c556262705f6f6b9a5057
WangSF <soullose@qq.com>
2025-09-11 15:56:52 +0800

    refactor(ArticleRepositoryImpl): 依赖注入 HttpClient
    - 将 `HttpClient` 实例通过构造函数注入到 `ArticleRepositoryImpl`
    - 在 `getArticles`, `getDefaultArticles`, 和 `getTopArticles` 方法中使用注入的 `HttpClient` 实例

--- lib/features/article/infrastructure/repositories/article_repository_impl.dart
+++ lib/features/article/infrastructure/repositories/article_repository_impl.dart
@@ -1,24 +1,31 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:wanandroid_app_flutter_riverpod/features/article/domain/entities/article_list.dart';
 
+import '../../../../core/net/http_client.dart';
 import '../../domain/repositories/article_repository.dart';
 
 class ArticleRepositoryImpl implements ArticleRepository {
-  ArticleRepositoryImpl();
+  final Ref _ref;
+
+  ArticleRepositoryImpl(this._ref);
 
   @override
   Future<List<Articles>> getArticles(int page) {
+    final httpManager = _ref.read(httpManagerProvider.notifier);
     // TODO: implement getArticles
     throw UnimplementedError();
   }
 
   @override
   Future<List<Articles>> getDefaultArticles() {
+    final httpManager = _ref.read(httpManagerProvider.notifier);
     // TODO: implement getDefaultArticles
     throw UnimplementedError();
   }
 
   @override
   Future<List<Articles>> getTopArticles() {
+    final httpManager = _ref.read(httpManagerProvider.notifier);
     // TODO: implement getTopArticles
     throw UnimplementedError();
   }
