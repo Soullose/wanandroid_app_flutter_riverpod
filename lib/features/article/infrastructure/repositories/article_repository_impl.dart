f19fd72e1519449c57c525504f025441283f8b19
WangSF <soullose@qq.com>
2025-09-11 16:01:42 +0800

    feat(article): 实现文章列表、默认文章和置顶文章的数据获取
    - 在 `ArticleRepositoryImpl` 中，为 `getArticles`、`getDefaultArticles` 和 `getTopArticles` 方法添加了通过 `httpManager` 获取数据的逻辑。
    - 使用 `ApiAddress` 来构建请求的 URL。
    - 将获取到的数据解析为 `PaginationData<Articles>` 对象。
    - 在调试模式下打印获取到的文章数据。
    - 移除了原有的 `UnimplementedError`。

--- lib/features/article/infrastructure/repositories/article_repository_impl.dart
+++ lib/features/article/infrastructure/repositories/article_repository_impl.dart
@@ -1,7 +1,11 @@
+import 'package:flutter/foundation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:wanandroid_app_flutter_riverpod/features/article/domain/entities/article_list.dart';
 
+import '../../../../core/constants/api_address.dart';
 import '../../../../core/net/http_client.dart';
+import '../../../../core/net/result_data.dart';
+import '../../../../model/pagination_data.dart';
 import '../../domain/repositories/article_repository.dart';
 
 class ArticleRepositoryImpl implements ArticleRepository {
@@ -10,22 +14,45 @@ class ArticleRepositoryImpl implements ArticleRepository {
   ArticleRepositoryImpl(this._ref);
 
   @override
-  Future<List<Articles>> getArticles(int page) {
+  Future<List<Articles>> getArticles(int page) async {
     final httpManager = _ref.read(httpManagerProvider.notifier);
+    ResultData? response =
+        await httpManager.netFetch(ApiAddress.articleUrl(pageNumber: page));
+    late PaginationData<Articles> articles = PaginationData<Articles>.fromJson(
+        response?.getData() as Map<String, dynamic>,
+        (json) => Articles.fromJson(json as Map<String, dynamic>));
+    if (kDebugMode) {
+      print('articles:${articles.datas}');
+    }
     // TODO: implement getArticles
     throw UnimplementedError();
   }
 
   @override
-  Future<List<Articles>> getDefaultArticles() {
+  Future<List<Articles>> getDefaultArticles() async {
     final httpManager = _ref.read(httpManagerProvider.notifier);
+    ResultData? response =
+        await httpManager.netFetch(ApiAddress.articleUrl(pageNumber: 0));
+    late PaginationData<Articles> articles = PaginationData<Articles>.fromJson(
+        response?.getData() as Map<String, dynamic>,
+        (json) => Articles.fromJson(json as Map<String, dynamic>));
+    if (kDebugMode) {
+      print('articles:${articles.datas}');
+    }
     // TODO: implement getDefaultArticles
     throw UnimplementedError();
   }
 
   @override
-  Future<List<Articles>> getTopArticles() {
+  Future<List<Articles>> getTopArticles() async {
     final httpManager = _ref.read(httpManagerProvider.notifier);
+    ResultData? response = await httpManager.netFetch(ApiAddress.topArticleUrl);
+    late PaginationData<Articles> articles = PaginationData<Articles>.fromJson(
+        response?.getData() as Map<String, dynamic>,
+        (json) => Articles.fromJson(json as Map<String, dynamic>));
+    if (kDebugMode) {
+      print('articles:${articles.datas}');
+    }
     // TODO: implement getTopArticles
     throw UnimplementedError();
   }
